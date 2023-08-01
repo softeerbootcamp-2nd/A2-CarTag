545ae4d3865a73f80f106f3fcac20325956f278a
김종원 Jongwon <tank3a@gmail.com>
2023-08-01 22:59:13 +0900

    [FEAT] #9 backend spring boot 프로젝트 제작

---
+++ backend/src/main/java/autoever2/cartag/CartagApplication.java
@@ -0,0 +1,13 @@
+package autoever2.cartag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartagApplication.class, args);
+	}
+
+}
