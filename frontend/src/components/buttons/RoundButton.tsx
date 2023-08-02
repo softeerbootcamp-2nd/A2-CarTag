24885f9f287604704342a658a2fc6ac65f7a989b
jijiseong <jiseong5036@gamil.com>
2023-08-02 16:32:36 +0900

    [FEAT] #25: inactive button 구현

--- frontend/src/components/buttons/RoundButton.tsx
+++ frontend/src/components/buttons/RoundButton.tsx
@@ -3,10 +3,11 @@ import { BodyKrRegular3 } from '../../styles/typefaces';
 
 interface IRoundButton extends React.HTMLAttributes<HTMLButtonElement> {
   type: 'option' | 'price';
+  inactive?: boolean;
 }
 
-export default function RoundButton({ type = 'option', ...props }: IRoundButton) {
-  return <Wrapper $type={type} {...props}></Wrapper>;
+export default function RoundButton({ type, inactive = false, ...props }: IRoundButton) {
+  return <Wrapper $incative={inactive} $type={type} {...props}></Wrapper>;
 }
 
 const priceHoverCss = css`
@@ -29,7 +30,7 @@ const optionActiveCss = css`
   background-color: ${({ theme }) => theme.color.primaryColor100};
 `;
 
-const Wrapper = styled.button<{ $type: 'option' | 'price' }>`
+const Wrapper = styled.button<{ $type: 'option' | 'price'; $incative: boolean }>`
   ${BodyKrRegular3}
   height: 36px;
   padding-left: 17px;
@@ -37,7 +38,14 @@ const Wrapper = styled.button<{ $type: 'option' | 'price' }>`
   border-radius: 18px;
   color: ${({ theme }) => theme.color.primaryColor};
 
-  ${({ $type, theme }) => {
+  ${({ $type, $incative, theme }) => {
+    if ($incative) {
+      return css`
+        border: 1px solid ${theme.color.gray200};
+        color: ${theme.color.gray600};
+      `;
+    }
+
     if ($type === 'price') {
       return css`
         border: 1px solid ${theme.color.primaryColor};
