3a56b78e99879571f13363a3136249ef9b3644c2
jijiseong <jiseong5036@gamil.com>
2023-08-03 10:43:55 +0900

    [FEAT] #26: DefaultCardStyle 컴포넌트 구현

---
+++ frontend/src/components/card/DefaultCardStyled.tsx
@@ -0,0 +1,38 @@
+import { css, styled } from 'styled-components';
+
+interface IDefaultCardStyled extends React.HTMLAttributes<HTMLDivElement> {
+  active: boolean;
+}
+
+export default function DefaultCardStyled({ active, ...props }: IDefaultCardStyled) {
+  return <Wrapper $active={active} {...props}></Wrapper>;
+}
+
+const activeCss = css`
+  background: ${({ theme }) => theme.color.cardBg};
+  border: 1px solid ${({ theme }) => theme.color.activeBlue};
+`;
+
+const inactiveCss = css`
+  &:hover {
+    border: 1px solid ${({ theme }) => theme.color.activeBlue};
+    background: ${({ theme }) => theme.color.cardBgOpacity05};
+  }
+  border: 1px solid ${({ theme }) => theme.color.gray200};
+  background: ${({ theme }) => theme.color.white};
+  color: ${({ theme }) => theme.color.gray500};
+`;
+
+const Wrapper = styled.div<{ $active: boolean }>`
+  ${({ $active }) => {
+    if ($active) {
+      return css`
+        ${activeCss}
+      `;
+    } else {
+      return css`
+        ${inactiveCss}
+      `;
+    }
+  }}
+`;
