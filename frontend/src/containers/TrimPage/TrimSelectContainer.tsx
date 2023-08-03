876499bfc452a18dfe96e544463c34597615716b
jijiseong <jiseong5036@gamil.com>
2023-08-03 16:32:28 +0900

    [FEAT] #65: trim 선택 영역 ui 작업

---
+++ frontend/src/containers/TrimPage/TrimSelectContainer.tsx
@@ -0,0 +1,87 @@
+import { styled } from 'styled-components';
+import CenterWrapper from '../../components/layout/CenterWrapper';
+import {
+  BodyKrRegular4,
+  HeadingEn4,
+  HeadingKrMedium5,
+  HeadingKrMedium7,
+} from '../../styles/typefaces';
+import DefaultCardStyled from '../../components/card/DefaultCardStyled';
+import { useState } from 'react';
+import RectButton from '../../components/buttons/RectButton';
+
+export default function TrimSelectContainer() {
+  const firstTrimIdx = 0;
+  const [selectedTrimIdx, setSelectedTrimIdx] = useState(firstTrimIdx);
+  const handleSelectedIdx = (idx: number) => {
+    setSelectedTrimIdx(idx);
+  };
+
+  return (
+    <Wrapper>
+      <Title>트림을 선택해주세요.</Title>
+      <TrimSection>
+        {/* Todo. map() 으로 데이터 받아서 만들기! */}
+        <TrimCard onClick={() => handleSelectedIdx(0)} active={selectedTrimIdx === 0 && true}>
+          <TrimDesc>기본기를 갖춘 베이직한 펠리세이드</TrimDesc>
+          <TrimTitle>Exclusive</TrimTitle>
+          <TrimPrice>100원!</TrimPrice>
+          <TrimButton type={'trim'}>선택하기</TrimButton>
+        </TrimCard>
+        <TrimCard onClick={() => handleSelectedIdx(1)} active={selectedTrimIdx === 1 && true}>
+          <TrimDesc>기본기를 갖춘 베이직한 펠리세이드</TrimDesc>
+          <TrimTitle>Exclusive</TrimTitle>
+          <TrimPrice>100원!</TrimPrice>
+          <TrimButton type={'trim'}>선택하기</TrimButton>
+        </TrimCard>
+        <TrimCard onClick={() => handleSelectedIdx(2)} active={selectedTrimIdx === 2 && true}>
+          <TrimDesc>기본기를 갖춘 베이직한 펠리세이드</TrimDesc>
+          <TrimTitle>Exclusive</TrimTitle>
+          <TrimPrice>100원!</TrimPrice>
+          <TrimButton type={'trim'}>선택하기</TrimButton>
+        </TrimCard>
+        <TrimCard onClick={() => handleSelectedIdx(3)} active={selectedTrimIdx === 3 && true}>
+          <TrimDesc>기본기를 갖춘 베이직한 펠리세이드</TrimDesc>
+          <TrimTitle>Exclusive</TrimTitle>
+          <TrimPrice>100원!</TrimPrice>
+          <TrimButton type={'trim'}>선택하기</TrimButton>
+        </TrimCard>
+      </TrimSection>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled(CenterWrapper)``;
+const Title = styled.div`
+  ${HeadingKrMedium5}
+  margin-top: 16px;
+`;
+
+const TrimSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 16px;
+  margin-top: 16px;
+`;
+
+const TrimCard = styled(DefaultCardStyled)`
+  padding: 20px 16px 12px 16px;
+  width: 100%;
+  height: 158px;
+  box-sizing: border-box;
+`;
+
+const TrimTitle = styled.div`
+  ${HeadingEn4}
+`;
+const TrimPrice = styled.div`
+  ${HeadingKrMedium7}
+  margin-top: 8px;
+`;
+const TrimDesc = styled.div`
+  ${BodyKrRegular4}
+`;
+const TrimButton = styled(RectButton)`
+  margin-top: 8px;
+  border-radius: 2px;
+`;
