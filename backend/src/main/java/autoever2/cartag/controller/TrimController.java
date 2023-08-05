27dadcd51f1eb836dd695822e2bce719fc893f35
dydwo0740 <80679257+dydwo0740@users.noreply.github.com>
2023-08-05 18:29:54 +0900

    [FEAT] #42 : car_type에 대한 트림 리스트를 반환하는 TrimController를 생성합니다.

---
+++ backend/src/main/java/autoever2/cartag/controller/TrimController.java
@@ -0,0 +1,38 @@
+package autoever2.cartag.controller;
+
+import autoever2.cartag.domain.dto.car.CarDto;
+import autoever2.cartag.service.CarService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cars")
+@RequiredArgsConstructor
+@Tag(name = "TrimController", description = "트림 반환 api 입니다.")
+public class TrimController {
+
+    private final CarService service;
+
+    @Operation(summary = "trim 조회", description = "trim 조회 method 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = CarDto.class))),
+    })
+    @GetMapping("/types")
+    public List<CarDto> carTrimInfo(@Parameter(description = "선택한 car_type") @RequestParam int carType) {
+        return service.findCarByCarType(carType);
+    }
+
+
+}
