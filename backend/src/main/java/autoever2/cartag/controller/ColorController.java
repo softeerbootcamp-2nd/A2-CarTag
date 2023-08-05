804e084960a29ce7bb8548a44c0624d971d3f6af
dydwo0740 <80679257+dydwo0740@users.noreply.github.com>
2023-08-05 18:56:04 +0900

    [FEAT] #36 : car_id에 따른 적절한 내장 색상을 반환하는 controller 생성

---
+++ backend/src/main/java/autoever2/cartag/controller/ColorController.java
@@ -0,0 +1,38 @@
+package autoever2.cartag.controller;
+
+import autoever2.cartag.domain.dto.colordto.InnerColorDto;
+import autoever2.cartag.domain.dto.colordto.OuterColorDto;
+import autoever2.cartag.service.ColorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cars")
+@Tag(name = "ColorController", description = "색상 반환 api 입니다.")
+public class ColorController {
+
+    private final ColorService service;
+
+
+    @Operation(summary = "차량 내장 색상 조회", description = "차량 내장 색상 조회 method 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = InnerColorDto.class))),
+    })
+    @GetMapping("/colors/inner")
+    public List<InnerColorDto> carInnerColorInfo(@Parameter(description = "선택한 car_id") @RequestParam int carId) {
+        return service.findInnerColorByCarId(carId);
+    }
+}
