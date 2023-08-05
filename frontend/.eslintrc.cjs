8231ee0d47f874e0b7bbc4e36112b8cc0d03b814
jijiseong <jiseong5036@gamil.com>
2023-08-05 23:13:35 +0900

    [CHORE] #74: eslint _ warning 무시 설정

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -12,5 +12,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
   },
 };
