4d0329c97a6bd357f51e0181ecfc3d388df4971b
dydwo0740 <80679257+dydwo0740@users.noreply.github.com>
2023-08-07 16:33:43 +0900

    [REFACTOR] #42 : defaultOption을 반환하는 DTO의 nullable 속성을 수정합니다.

--- backend/src/main/java/autoever2/cartag/domain/dto/car/DefaultOptionDto.java
+++ backend/src/main/java/autoever2/cartag/domain/dto/car/DefaultOptionDto.java
@@ -1,12 +1,19 @@
 package autoever2.cartag.domain.dto.car;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter @Builder
+@Getter
+@Builder
+@Schema(description = "차량 DefaultOption 반환")
 public class DefaultOptionDto {
+    @Schema(description = "option 이름")
     private String optionName;
+    @Schema(description = "option image의 url")
     private String optionImage;
+    @Schema(description = "option에 대한 설명")
     private String optionDescription;
+    @Schema(description = "option의 구매 횟수")
     private int defaultOptionCount;
 }
