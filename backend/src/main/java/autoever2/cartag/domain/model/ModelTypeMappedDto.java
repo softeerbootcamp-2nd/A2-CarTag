9cb2fd138bdfa9db424051a69f7d4ac0cdeac943
김종원 Jongwon <tank3a@gmail.com>
2023-08-07 21:51:36 +0900

    [REFACTOR] #34: Repository에서 받는 Data를 DTO로 받도록 코드 스타일 통합

--- backend/src/main/java/autoever2/cartag/domain/model/ModelTypeMappedDto.java
+++ backend/src/main/java/autoever2/cartag/domain/model/ModelTypeMappedDto.java
@@ -1,11 +1,15 @@
-package autoever2.cartag.repository.model;
+package autoever2.cartag.domain.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class ModelTypeMappedDto {
 
     private int modelId;
@@ -31,10 +35,5 @@ public class ModelTypeMappedDto {
         if (o == null || getClass() != o.getClass()) return false;
         ModelTypeMappedDto that = (ModelTypeMappedDto) o;
         return modelId == that.modelId && isDefaultOption == that.isDefaultOption && Objects.equals(modelName, that.modelName) && Objects.equals(modelTypeName, that.modelTypeName) && Objects.equals(modelPrice, that.modelPrice) && Objects.equals(modelBoughtCount, that.modelBoughtCount);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(modelId, modelName, modelTypeName, modelPrice, modelBoughtCount, isDefaultOption);
     }
 }
