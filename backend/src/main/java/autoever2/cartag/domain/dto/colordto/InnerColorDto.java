b7716f5b4ad60bcd813a5fc5930b8d350c4f094e
dydwo0740 <80679257+dydwo0740@users.noreply.github.com>
2023-08-07 21:21:42 +0900

    [REFACTOR] #26 : 차량 내장 색상에 대해서 repository영역에서 한번에 반환합니다.

--- backend/src/main/java/autoever2/cartag/domain/dto/colordto/InnerColorDto.java
+++ backend/src/main/java/autoever2/cartag/domain/dto/colordto/InnerColorDto.java
@@ -1,13 +1,11 @@
 package autoever2.cartag.domain.dto.colordto;
 
-import autoever2.cartag.domain.entity.color.Color;
-import autoever2.cartag.domain.entity.color.ColorCarMapper;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
-@Builder
+@Setter
 @Schema(description = "차량 내부 색상 반환 DTO")
 public class InnerColorDto {
     @Schema(description = "내부 색상 이름")
@@ -20,14 +18,4 @@ public class InnerColorDto {
     private Long colorBoughtCount;
     @Schema(description = "차량에 적용된 내부 색상 이미지 url")
     private String colorCarImage;
-
-    public static InnerColorDto toDto(ColorCarMapper colorCarMapper, Color color) {
-        return InnerColorDto.builder()
-                .colorName(color.getColorName())
-                .colorImage(color.getColorImage())
-                .colorPrice(colorCarMapper.getColorPrice())
-                .colorBoughtCount(colorCarMapper.getColorBoughtCount())
-                .colorCarImage(colorCarMapper.getColorCarImage())
-                .build();
-    }
 }
