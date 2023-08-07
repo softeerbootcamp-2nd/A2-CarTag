e0593b02e440989923700300c6c7639fe6151c9f
dydwo0740 <80679257+dydwo0740@users.noreply.github.com>
2023-08-07 16:31:22 +0900

    [REFACTOR] #36 :  내장 색상을 반환하는 DTO의 nullable 속성을 수정합니다.

--- backend/src/main/java/autoever2/cartag/domain/dto/colordto/InnerColorDto.java
+++ backend/src/main/java/autoever2/cartag/domain/dto/colordto/InnerColorDto.java
@@ -10,15 +10,15 @@ import lombok.Getter;
 @Builder
 @Schema(description = "차량 내부 색상 반환 DTO")
 public class InnerColorDto {
-    @Schema(description = "내부 색상 이름", nullable = true)
+    @Schema(description = "내부 색상 이름")
     private String colorName;
-    @Schema(description = "내부 색상 이미지 url", nullable = true)
+    @Schema(description = "내부 색상 이미지 url")
     private String colorImage;
-    @Schema(description = "내부 색상 이미지 가격", nullable = true)
+    @Schema(description = "내부 색상 이미지 가격")
     private Long colorPrice;
-    @Schema(description = "판매된 내부 색상 이미지", nullable = true)
+    @Schema(description = "판매된 내부 색상 이미지")
     private Long colorBoughtCount;
-    @Schema(description = "차량에 적용된 내부 색상 이미지 url", nullable = true)
+    @Schema(description = "차량에 적용된 내부 색상 이미지 url")
     private String colorCarImage;
 
     public static InnerColorDto toDto(ColorCarMapper colorCarMapper, Color color) {
