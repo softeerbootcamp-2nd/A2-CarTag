52efdb522b8511027ccd42cf03b11a0d93c686ff
dydwo0740 <80679257+dydwo0740@users.noreply.github.com>
2023-08-08 18:41:20 +0900

    [FEAT] #36 : 내장 색상을 위해서 테스트 코드 및 sql문을 작성합니다."

--- backend/src/test/java/autoever2/cartag/repository/CarRepositoryTest.java
+++ backend/src/test/java/autoever2/cartag/repository/CarRepositoryTest.java
@@ -2,35 +2,43 @@ package autoever2.cartag.repository;
 
 import autoever2.cartag.domain.car.CarInfoDto;
 import autoever2.cartag.domain.car.DefaultOptionDto;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
+@ActiveProfiles("test")
+@Sql(scripts = {"classpath:/insert/insertCar-h2.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = {"/cleanup.sql"}, executionPhase =  Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class CarRepositoryTest {
 
     @Autowired
     private CarRepository repository;
 
+
     @Test
     @DisplayName("CarType 별 트림 리스트를 반환합니다.")
-    void findCars(){
+    void findCars() {
         List<CarInfoDto> carByCarType = repository.findCarByCarType(1);
-
-        Assertions.assertEquals(carByCarType.size(), 4);
+        assertEquals(4, carByCarType.size());
+        assertEquals("Le Blanc", carByCarType.get(0).getTrim());
+        assertEquals(40000000, carByCarType.get(0).getCarDefaultPrice());
     }
 
     @Test
     @DisplayName("carId에 해당하는 모든 defaultOption을 가져옵니다.")
-    void findDefaultOptions(){
+    void findDefaultOptions() {
         List<DefaultOptionDto> defaultOptionByCarId = repository.findDefaultOptionByCarId(1);
-
-        Assertions.assertEquals(defaultOptionByCarId.size(), 3);
+        assertEquals(3, defaultOptionByCarId.size());
+        assertEquals("image_1", defaultOptionByCarId.get(0).getOptionName());
     }
-
 
 }
