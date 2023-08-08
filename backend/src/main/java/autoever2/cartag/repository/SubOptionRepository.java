801bc74366e6a4bf855044f3e664a8b9300075e7
김종원 Jongwon <tank3a@gmail.com>
2023-08-08 15:09:53 +0900

    [FEAT] #38: SubOption 리스트 호출 API 관련 Controller-Service-Repo 생성

---
+++ backend/src/main/java/autoever2/cartag/repository/SubOptionRepository.java
@@ -0,0 +1,67 @@
+package autoever2.cartag.repository;
+
+import autoever2.cartag.domain.suboption.SubOptionMappedDto;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class SubOptionRepository {
+
+    private final NamedParameterJdbcTemplate template;
+
+    public SubOptionRepository(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    public List<SubOptionMappedDto> findAllSubOptionWithCategoryNameByCarId(int carId) {
+        String sql = "select o.sub_option_id, o.option_name, oc.option_category_name, o.option_image, ob.option_bought_count, ob.option_used_count, o.option_price " +
+                "from suboptiondata ob " +
+                "inner join suboption o " +
+                "on o.sub_option_id = ob.sub_option_id " +
+                "inner join optioncategory oc " +
+                "on oc.option_category_id = o.category_id " +
+                "where ob.car_id = :carId";
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("carId", carId);
+
+        return template.query(sql, param, subOptionMapper());
+    }
+
+    private RowMapper<SubOptionMappedDto> subOptionMapper() {
+        return BeanPropertyRowMapper.newInstance(SubOptionMappedDto.class);
+    }
+
+    public Optional<Long> findCarBoughtCountByCarId(int carId) {
+        String sql = "select bought_count from car where car_id = :carId";
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("carId", carId);
+
+        try {
+            return Optional.of(template.queryForObject(sql, param, Long.class));
+        } catch (DataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    public List<String> findAllHashtagNameBySubOptionId(int subOptionId) {
+        String sql = "select h.hashtag_name " +
+                "from optionhashtag oh, hashtag h " +
+                "where oh.hashtag_id = h.hashtag_id and oh.sub_option_id = :subOptionId";
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("subOptionId", subOptionId);
+
+        return template.queryForList(sql, param, String.class);
+    }
+}
---
+++ backend/src/main/java/autoever2/cartag/service/SubOptionService.java
@@ -0,0 +1,44 @@
+package autoever2.cartag.service;
+
+import autoever2.cartag.domain.suboption.SubOptionDto;
+import autoever2.cartag.domain.suboption.SubOptionMappedDto;
+import autoever2.cartag.repository.SubOptionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SubOptionService {
+
+    private final SubOptionRepository subOptionRepository;
+
+    public List<SubOptionDto> getSubOptionList(int carId) {
+        List<SubOptionMappedDto> subOptionList = subOptionRepository.findAllSubOptionWithCategoryNameByCarId(carId);
+        Long carBoughtCount = subOptionRepository.findCarBoughtCountByCarId(carId).orElse(0L);
+
+        return subOptionList.stream().map(subOptionMappedDto -> {
+                    int percentage = 0;
+                    if (carBoughtCount != 0) {
+                        percentage = (int) (subOptionMappedDto.getOptionBoughtCount() * 100 / carBoughtCount);
+                    }
+
+                    boolean hasHmgData = subOptionMappedDto.getOptionUsedCount() != 0;
+
+                    return SubOptionDto.builder()
+                            .subOptionId(subOptionMappedDto.getSubOptionId())
+                            .optionCategoryName(subOptionMappedDto.getOptionCategoryName())
+                            .optionImage(subOptionMappedDto.getOptionImage())
+                            .optionPrice(subOptionMappedDto.getOptionPrice())
+                            .optionName(subOptionMappedDto.getOptionName())
+                            .percentage(percentage)
+                            .hashtagName(subOptionRepository.findAllHashtagNameBySubOptionId(subOptionMappedDto.getSubOptionId()))
+                            .hasHmgData(hasHmgData)
+                            .build();
+                }
+        ).collect(Collectors.toList());
+    }
+
+}
