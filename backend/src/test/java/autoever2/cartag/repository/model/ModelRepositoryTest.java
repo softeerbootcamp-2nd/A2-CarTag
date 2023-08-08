b28dfadc7b04cbbc6ce343d104098127bd482fbb
dydwo0740 <80679257+dydwo0740@users.noreply.github.com>
2023-08-08 18:40:46 +0900

    [FEAT] #37 : 외장 색상을 위해서 테스트 코드 작성을 합니다."

--- backend/src/test/java/autoever2/cartag/repository/model/ModelRepositoryTest.java
+++ backend/src/test/java/autoever2/cartag/repository/model/ModelRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
