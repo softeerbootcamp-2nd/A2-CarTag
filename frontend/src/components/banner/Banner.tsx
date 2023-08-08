b49d5c18ff39a2eec06371ca10d03010fb0de350
jijiseong <jiseong5036@gamil.com>
2023-08-08 16:49:19 +0900

    [STYLE] #97: 배너 min-height 추가

--- frontend/src/components/banner/Banner.tsx
+++ frontend/src/components/banner/Banner.tsx
@@ -3,8 +3,8 @@ import { BodyKrRegular3, BodyKrRegular4, HeadingKrBold1 } from '../../styles/typ
 import CenterWrapper from '../layout/CenterWrapper';
 
 interface IBanner extends React.HTMLAttributes<HTMLDivElement> {
-  subtitle: string;
-  title: string;
+  subtitle?: string;
+  title?: string;
   desc?: string;
 }
 
@@ -14,9 +14,9 @@ export default function Banner({ subtitle, title, desc, ...props }: IBanner) {
       <BannerBg {...props}>
         <CenterWrapper>
           <InfoWrapper>
-            <SubTitle>{subtitle}</SubTitle>
-            <Title>{title}</Title>
-            <AdditionalText>{desc}</AdditionalText>
+            {subtitle && <SubTitle>{subtitle}</SubTitle>}
+            {title && <Title>{title}</Title>}
+            {desc && <AdditionalText>{desc}</AdditionalText>}
           </InfoWrapper>
         </CenterWrapper>
         {props.children}
@@ -28,7 +28,7 @@ export default function Banner({ subtitle, title, desc, ...props }: IBanner) {
 const BannerBg = styled.div`
   position: relative;
   width: 100%;
-  height: 360px;
+  min-height: 360px;
   background: linear-gradient(
       180deg,
       rgba(162, 199, 231, 0.2) 24.92%,
