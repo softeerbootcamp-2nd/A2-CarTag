6d6113c0cfba8b61012734f48d0dc6db4cb044d8
jijiseong <jiseong5036@gamil.com>
2023-08-10 17:21:45 +0900

    [FEAT] :현대 url 상수 추가

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const MAX_PAGE = 3;
 export const NUM_IN_A_PAGE = 4;
 export const MAX_TEXT_CNT = 98;
-Object.freeze({ MAX_PAGE, NUM_IN_A_PAGE, MAX_TEXT_CNT });
+export const HYUNDAI_URL = 'https://www.hyundai.com/kr/ko/e';
+Object.freeze({ MAX_PAGE, NUM_IN_A_PAGE, MAX_TEXT_CNT, HYUNDAI_URL });
