0cbe176baca3262fd64085c174c8cb3656a6996d
jijiseong <jiseong5036@gamil.com>
2023-08-10 21:26:53 +0900

    [FEAT] #96: 동글 로딩 컴포넌트 구현

---
+++ frontend/src/components/loading/Loading.tsx
@@ -0,0 +1,58 @@
+import { keyframes, styled } from 'styled-components';
+import { BodyKrMedium3 } from '../../styles/typefaces';
+import { LoadingIcon, LoadingIcon2, LoadingIcon3 } from '../common/icons/Icons';
+import { flexCenterCss } from '../../utils/commonStyle';
+
+export default function Loading() {
+  return (
+    <Wrapper>
+      <Spinner>
+        <AbsoluteCenter>
+          <LoadingIcon></LoadingIcon>
+        </AbsoluteCenter>
+        <AbsoluteCenter>
+          <LoadingIcon2></LoadingIcon2>
+        </AbsoluteCenter>
+        <AbsoluteCenter>
+          <LoadingIcon3 />
+        </AbsoluteCenter>
+      </Spinner>
+      <Text>데이터를 불러오는 중입니다.</Text>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const spin = keyframes`
+    to{
+        transform: rotate(360deg);
+    }
+`;
+
+const Spinner = styled.div`
+  position: relative;
+  ${flexCenterCss}
+  width: 54px;
+  height: 54px;
+  border: 3px solid ${({ theme }) => theme.color.gray200};
+  border-radius: 50%;
+  border-top-color: ${({ theme }) => theme.color.activeBlue};
+  animation: ${spin} 1.5s linear infinite;
+`;
+
+const Text = styled.div`
+  ${BodyKrMedium3};
+  width: 200px;
+  text-align: center;
+  margin-top: 16px;
+`;
+
+const AbsoluteCenter = styled.div`
+  position: absolute;
+  ${flexCenterCss}
+`;
