bf3ca37d4884f31cf1383bc9a415f76f868119c9
jijiseong <jiseong5036@gamil.com>
2023-08-13 21:49:03 +0900

    [FEAT] #205: GPT로 대충 useFetch 작성

---
+++ frontend/src/hooks/useFetch.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+interface FetchResponse<T> {
+  data: T | null;
+  loading: boolean;
+  error: Error | null;
+}
+
+export function useFetch<T>(url: string): FetchResponse<T> {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+
+        const responseData: T = await response.json();
+        setData(responseData);
+      } catch (err) {
+        setError(err as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
--- frontend/src/pages/TrimPage.tsx
+++ frontend/src/pages/TrimPage.tsx
@@ -1,7 +1,12 @@
 import TrimBannerContainer from '../containers/TrimPage/TrimBannerContainer';
 import TrimSelectContainer from '../containers/TrimPage/TrimSelectContainer';
+import { useFetch } from '../hooks/useFetch';
+import { TRIM_API } from '../utils/apis';
 
 export default function TrimPage() {
+  const { data, loading } = useFetch(TRIM_API + `carType=${1}`);
+
+  console.log(data, loading);
   return (
     <>
       <TrimBannerContainer />
