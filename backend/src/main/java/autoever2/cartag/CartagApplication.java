a6bb0b64b25b7623f37cca14daa276baa51be608
이용재 <80679257+dydwo0740@users.noreply.github.com>
2023-08-17 13:13:43 +0900

    Update CartagApplication.java

--- backend/src/main/java/autoever2/cartag/CartagApplication.java
+++ backend/src/main/java/autoever2/cartag/CartagApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CartagApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CartagApplication.class, args);
