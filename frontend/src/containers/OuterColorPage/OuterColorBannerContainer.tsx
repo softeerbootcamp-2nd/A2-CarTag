94fedb688fd62196c3c51437bd423268a23599e9
jijiseong <jiseong5036@gamil.com>
2023-08-17 22:28:39 +0900

    [FIX] #294: 이미지 로딩 오류 수정

--- frontend/src/containers/OuterColorPage/OuterColorBannerContainer.tsx
+++ frontend/src/containers/OuterColorPage/OuterColorBannerContainer.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, useCallback, useContext, useEffect, useRef, useState } from 'react';
+import { MouseEventHandler, useCallback, useContext, useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import Banner from '../../components/common/banner/Banner';
 import CenterWrapper from '../../components/layout/CenterWrapper';
@@ -8,12 +8,8 @@ import { IMG_URL, OUTER_IMG_API } from '../../utils/apis';
 import Loading from '../../components/loading/Loading';
 import { OuterColorContext } from '../../context/OuterColorProvider';
 
-interface IImgLoadingList {
-  [key: number]: boolean;
-}
-
 export default function OuterColorBannerContainer() {
-  const { data: outerColorData, seletedColorId } = useContext(OuterColorContext);
+  const { seletedColorId } = useContext(OuterColorContext);
   const { data: car360ImgUrls, loading } = useFetch<string[]>(
     `${OUTER_IMG_API}?colorid=${seletedColorId}`
   );
@@ -22,9 +18,8 @@ export default function OuterColorBannerContainer() {
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
   const [startIdx, setStartIdx] = useState(0);
-  const [imgLoadingList, setImgLoadingList] = useState<IImgLoadingList>({});
+  const [imgLoading, setImgLoading] = useState(true);
   const imgLen = car360ImgUrls ? car360ImgUrls.length : 0;
-  const prevCar360ImgUrls = useRef<string[]>([]);
 
   const handleMousedown: MouseEventHandler<HTMLDivElement> = ({ pageX }) => {
     setIsDragging(true);
@@ -49,53 +44,49 @@ export default function OuterColorBannerContainer() {
     setStartX(pageX);
   };
 
-  const initImgLoadingState = useCallback(() => {
-    if (!outerColorData) return;
-    const dataLen = outerColorData.length;
-    const initLoadingList: IImgLoadingList = {};
-    for (let i = 3; i < dataLen + 3; i++) {
-      initLoadingList[i] = true;
+  const isLoadComplete = useCallback((urls: string[]) => {
+    for (const url of urls) {
+      if (!localStorage.getItem(url)) {
+        return false;
+      }
     }
-    setImgLoadingList(initLoadingList);
-  }, [outerColorData, setImgLoadingList]);
+    return true;
+  }, []);
 
   const downloadAndSaveImages = useCallback(
-    async (car360ImgUrls: string[]) => {
-      const isChangeImgUrls = prevCar360ImgUrls.current === car360ImgUrls;
-      if (isChangeImgUrls) return;
-      const imgBlob = await Promise.all(
+    async (car360ImgUrls: string[], abortController: AbortController) => {
+      if (isLoadComplete(car360ImgUrls)) {
+        setImgLoading(false);
+        return;
+      }
+      setImgLoading(true);
+
+      await Promise.all(
         car360ImgUrls.map(async (url, idx) => {
           const isImageExist = localStorage.getItem(url) !== null;
           if (isImageExist) return;
-          const fetchUrl = `${IMG_URL}${url}`;
-          const res = await fetch(fetchUrl);
+          const res = await fetch(IMG_URL + url, {
+            signal: abortController.signal,
+          });
           const imgBlob = await res.blob();
-          const imageUrl = car360ImgUrls[idx];
-          localStorage.setItem(imageUrl, URL.createObjectURL(imgBlob));
+          const localStorageKey = car360ImgUrls[idx];
+          localStorage.setItem(localStorageKey, URL.createObjectURL(imgBlob));
           return imgBlob;
         })
       );
-
-      if (imgBlob) {
-        setImgLoadingList((cur) => {
-          return { ...cur, [seletedColorId]: false };
-        });
-      }
+      setImgLoading(false);
     },
-    [setImgLoadingList, seletedColorId]
+    [isLoadComplete]
   );
-
-  useEffect(initImgLoadingState, [initImgLoadingState]);
 
   useEffect(() => {
     if (!car360ImgUrls || loading) return;
-    downloadAndSaveImages(car360ImgUrls);
+    const abortController = new AbortController();
+    downloadAndSaveImages(car360ImgUrls, abortController);
+    return () => {
+      abortController.abort();
+    };
   }, [downloadAndSaveImages, loading, car360ImgUrls]);
-
-  useEffect(() => {
-    if (!car360ImgUrls) return;
-    prevCar360ImgUrls.current = car360ImgUrls;
-  }, [car360ImgUrls]);
 
   const car360Components = car360ImgUrls?.map((url, idx) => {
     const imgSrc = localStorage.getItem(url);
@@ -111,7 +102,7 @@ export default function OuterColorBannerContainer() {
             <CarShadow>
               <DegreeCaption>360°</DegreeCaption>
             </CarShadow>
-            {imgLoadingList[seletedColorId] ? <Loading /> : car360Components}
+            {imgLoading ? <Loading /> : car360Components}
           </ImgWrapper>
         </FlexCenterWrapper>
       </OuterColorBanner>
