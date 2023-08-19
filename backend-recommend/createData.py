b23f0856f2aac5de1f9971bd4971fa383773b3ed
김종원 Jongwon <tank3a@gmail.com>
2023-08-19 16:14:07 +0900

    [REFACTOR] #318: 견적 데이터 생성 코드 옵션 ID를 선택 옵션 ID로 변경

--- backend-recommend/createData.py
+++ backend-recommend/createData.py
@@ -22,39 +22,40 @@ dict[5] = [2,3,6]
 dict[6] = [2,4,5]
 dict[7] = [2,4,6]
 
-numbers = list(range(1, 16))  # 1부터 15까지의 숫자 리스트
-for selected_count in range(14):   # 뽑을 숫자의 개수 # 특정 숫자
-  for comb in combinations(numbers, selected_count):
-      if 13 in comb and 14 in comb:
-          continue
-      if 13 in comb and 15 in comb:
-        continue
-      if 14 in comb and 15 in comb:
-        continue
-      subLists = list(comb)
-      string_S = ""
-      for i in range(len(subLists)):
-        sub = str(subLists[i])
-        string_S += sub
-        if(i == len(subLists) - 1):
-          break
-        string_S +=  ","
-      for i in range(8):
-        all_combinations = []
-        subsub = dict[i]
-        all_combinations.append(subsub[0])
-        all_combinations.append(subsub[1])
-        all_combinations.append(subsub[2])
-        all_combinations.append(string_S)
-        if len(subLists) == 0 or len(subLists) == 1 or len(subLists) == 12 or len(subLists) == 13:
-          all_combinations.append(int(np.random.normal(meanV1, std_dev)))
-        elif len(subLists) == 2 or len(subLists) == 3 or len(subLists) == 10 or len(subLists) == 11:
-          all_combinations.append(int(np.random.normal(meanV2, std_dev)))
-        elif len(subLists) == 4 or len(subLists) == 5 or len(subLists) == 8 or len(subLists) == 9:
-          all_combinations.append(int(np.random.normal(meanV3, std_dev)))
-        else:
-          all_combinations.append(int(np.random.normal(mean, std_dev)))
-        newData.append(all_combinations)
+numbers = []
+numbers.extend((69,70,71,72,73,74,84,85,86,87,88,89,90,91,92))  # 1부터 15까지의 숫자 리스트
+for selected_count in range(13):   # 뽑을 숫자의 개수 # 특정 숫자
+    for comb in combinations(numbers, selected_count):
+        if 90 in comb and 91 in comb:
+            continue
+        if 90 in comb and 92 in comb:
+            continue
+        if 91 in comb and 92 in comb:
+            continue
+        subLists = list(comb)
+        string_S = ""
+        for i in range(len(subLists)):
+            sub = str(subLists[i])
+            string_S += sub
+            if(i == len(subLists) - 1):
+                break
+            string_S +=  ","
+        for i in range(8):
+            all_combinations = []
+            subsub = dict[i]
+            all_combinations.append(subsub[0])
+            all_combinations.append(subsub[1])
+            all_combinations.append(subsub[2])
+            all_combinations.append(string_S)
+            if len(subLists) == 0 or len(subLists) == 1 or len(subLists) == 12 or len(subLists) == 13:
+                all_combinations.append(int(np.random.normal(meanV1, std_dev)))
+            elif len(subLists) == 2 or len(subLists) == 3 or len(subLists) == 10 or len(subLists) == 11:
+                all_combinations.append(int(np.random.normal(meanV2, std_dev)))
+            elif len(subLists) == 4 or len(subLists) == 5 or len(subLists) == 8 or len(subLists) == 9:
+                all_combinations.append(int(np.random.normal(meanV3, std_dev)))
+            else:
+                all_combinations.append(int(np.random.normal(mean, std_dev)))
+            newData.append(all_combinations)
 
 df = pd.DataFrame(newData, columns=['PowerTrain', 'BodyType', 'Operation', 'CarOptionList', 'count'])
 df.index = df.index + 1
