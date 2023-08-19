d9504d3c15ecb519743253452a84fec8f2382cc1
jijiseong <jiseong5036@gamil.com>
2023-08-19 23:00:56 +0900

    [FEAT] #332: 결과 페이지 애니메이션 수정

--- frontend/src/containers/ResultPage/DetailContainer.tsx
+++ frontend/src/containers/ResultPage/DetailContainer.tsx
@@ -2,7 +2,7 @@ import { styled } from 'styled-components';
 import { HeadingKrMedium7 } from '../../styles/typefaces';
 import Details from '../../components/details/Details';
 import SummaryItem from '../../components/details/SummaryItem';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { ItemContext } from '../../context/ItemProvider';
 
 export default function DetailContainer() {
@@ -13,16 +13,32 @@ export default function DetailContainer() {
     optionPrice: 0,
   });
   const [isOpen, setIsOepn] = useState<{ [key: number]: boolean }>({
+    0: true,
     1: true,
     2: true,
-    3: true,
+    3: false,
     4: false,
     5: false,
     6: false,
     7: false,
-    8: false,
+  });
+  const itemListRef0 = useRef<HTMLUListElement>(null);
+  const itemListRef1 = useRef<HTMLUListElement>(null);
+  const itemListRef2 = useRef<HTMLUListElement>(null);
+  const [itemListHeight, setItemListHeight] = useState<{ [key: number]: number }>({
+    0: 0,
+    1: 0,
+    2: 0,
   });
 
+  console.log(itemListHeight);
+  useEffect(() => {
+    setItemListHeight({
+      0: itemListRef0.current ? itemListRef0.current.getBoundingClientRect().height : 0,
+      1: itemListRef1.current ? itemListRef1.current.getBoundingClientRect().height : 0,
+      2: itemListRef2.current ? itemListRef2.current.getBoundingClientRect().height : 0,
+    });
+  }, []);
   const setOpenedIdx = (idx: number) => {
     setIsOepn((cur) => {
       const copy = { ...cur };
@@ -64,10 +80,10 @@ export default function DetailContainer() {
       <Details
         desc={`+ ${detailPrice.modelTypePrice.toLocaleString()} 원`}
         title="모델 선택"
-        open={isOpen[1]}
-        onClick={() => setOpenedIdx(1)}
+        open={isOpen[0]}
+        onClick={() => setOpenedIdx(0)}
       >
-        <ItemList $open={isOpen[1]}>
+        <ItemList $open={isOpen[0]} $height={itemListHeight[0]} ref={itemListRef0}>
           <SummaryItem
             imgSrc={selectedItem.modelType.powerTrain.imgSrc}
             itemName={selectedItem.modelType.powerTrain.title}
@@ -91,10 +107,10 @@ export default function DetailContainer() {
       <Details
         desc={`+ ${detailPrice.colorPrice.toLocaleString()} 원`}
         title="색상"
-        open={isOpen[2]}
-        onClick={() => setOpenedIdx(2)}
+        open={isOpen[1]}
+        onClick={() => setOpenedIdx(1)}
       >
-        <ItemList $open={isOpen[2]}>
+        <ItemList $open={isOpen[1]} $height={itemListHeight[1]} ref={itemListRef1}>
           <SummaryItem
             imgSrc={selectedItem.innerColor.imgSrc}
             itemName={selectedItem.innerColor.title}
@@ -112,31 +128,33 @@ export default function DetailContainer() {
       <Details
         desc={`+ ${detailPrice.optionPrice.toLocaleString()} 원`}
         title="추가 옵션"
-        open={isOpen[3]}
-        onClick={() => setOpenedIdx(3)}
+        open={isOpen[2]}
+        onClick={() => setOpenedIdx(2)}
       >
-        <ItemList $open={isOpen[3]}>{optionSummaryItems}</ItemList>
+        <ItemList $open={isOpen[2]} $height={itemListHeight[2]} ref={itemListRef2}>
+          {optionSummaryItems}
+        </ItemList>
       </Details>
-      <Details desc="- 0원" title="탁송" open={isOpen[4]} onClick={() => setOpenedIdx(4)}></Details>
+      <Details desc="- 0원" title="탁송" open={isOpen[3]} onClick={() => setOpenedIdx(3)}></Details>
       <Details
         desc="-0 원"
         title="할인 및 포인트"
-        open={isOpen[5]}
-        onClick={() => setOpenedIdx(5)}
+        open={isOpen[4]}
+        onClick={() => setOpenedIdx(4)}
       ></Details>
       <Details
         desc="결제수단을 선택하고 지불조건 및 납입사항을 확인하세요."
         title="결제 수단"
-        open={isOpen[6]}
-        onClick={() => setOpenedIdx(6)}
+        open={isOpen[5]}
+        onClick={() => setOpenedIdx(5)}
       ></Details>
       <Details
         desc="할인/포인트 및 결제 방법 선택 후 확인 가능해요."
         title="면세 구분 및 등록비"
-        open={isOpen[7]}
-        onClick={() => setOpenedIdx(7)}
+        open={isOpen[6]}
+        onClick={() => setOpenedIdx(6)}
       ></Details>
-      <Details desc="" title="안내사항" open={isOpen[8]} onClick={() => setOpenedIdx(8)}></Details>
+      <Details desc="" title="안내사항" open={isOpen[7]} onClick={() => setOpenedIdx(7)}></Details>
     </Wrapper>
   );
 }
@@ -149,11 +167,12 @@ const Title = styled.div`
   ${HeadingKrMedium7}
 `;
 
-const ItemList = styled.ul<{ $open: boolean }>`
+const ItemList = styled.ul<{ $open: boolean; $height?: number }>`
   :last-child {
     border-bottom: none;
   }
 
-  max-height: ${({ $open }) => ($open ? '500px' : '0px')};
-  transition: all 0.3s ease;
+  transition: 0.3s ease;
+  margin-top: ${({ $open, $height }) => ($open ? `-${$height}px` : '0px')};
+  opacity: ${({ $open }) => ($open ? 0 : 1)};
 `;
