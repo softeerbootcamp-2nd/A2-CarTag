975cc3e1a3b2bdd65328d1507654864e6582be22
dydwo0740 <80679257+dydwo0740@users.noreply.github.com>
2023-08-20 13:41:20 +0900

    [REFACTOR] #346 : dto에 필드를 추가하고, 필요 없는 dto를 삭제합니다.

--- backend/src/main/java/autoever2/cartag/domain/model/ModelDefaultDto.java
+++ backend/src/main/java/autoever2/cartag/domain/model/ModelDefaultDto.java
@@ -8,12 +8,14 @@ import lombok.Setter;
 @Getter @Setter
 @NoArgsConstructor
 public class ModelDefaultDto {
+    private int modelId;
     private String modelName;
     private Long modelPrice;
     private String modelImage;
 
     @Builder
-    public ModelDefaultDto(String modelName, Long modelPrice, String modelImage) {
+    public ModelDefaultDto(int modelId, String modelName, Long modelPrice, String modelImage) {
+        this.modelId = modelId;
         this.modelName = modelName;
         this.modelPrice = modelPrice;
         this.modelImage = modelImage;
