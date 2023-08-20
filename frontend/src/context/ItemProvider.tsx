3712f1ca234dcff3635bf9715b7da0c09b4940dc
jijiseong <jiseong5036@gamil.com>
2023-08-20 14:11:11 +0900

    [FEAT] #343: 트림 선택 시 기본값 설정

--- frontend/src/context/ItemProvider.tsx
+++ frontend/src/context/ItemProvider.tsx
@@ -1,27 +1,53 @@
 import { ReactNode, createContext, useReducer, useState } from 'react';
+import itemReducer, { actionType } from '../reducer/itemReducer';
 
-type defaultItemType = {
+export type defaultItemType = {
   id: number;
   name: string;
   price: number;
 };
-interface detailItemType extends defaultItemType {
+export interface detailItemType extends defaultItemType {
   title: string;
   imgSrc: string;
 }
-interface IOuterColorItemType extends detailItemType {
+export interface IColorItemType extends detailItemType {
   carImgSrc: string;
 }
-interface ISelectedItem {
+export interface ISelectedItem {
   trim: defaultItemType;
   modelType: {
     powerTrain: detailItemType;
     bodyType: detailItemType;
     operation: detailItemType;
   };
-  innerColor: detailItemType;
-  outerColor: IOuterColorItemType;
+  innerColor: IColorItemType;
+  outerColor: IColorItemType;
   options: detailItemType[];
+}
+
+export interface IDefaultInfo {
+  powerTrainId: number;
+  powerTrainName: string;
+  powerTrainImage: string;
+  powerTrainPrice: number;
+  bodyTypeId: number;
+  bodyTypeName: string;
+  bodyTypeImage: string;
+  bodyTypePrice: number;
+  operationId: number;
+  operationName: string;
+  operationImage: string;
+  operationPrice: number;
+  colorOuterId: number;
+  colorOuterImage: string;
+  colorCarOuterImage: string;
+  colorOuterPrice: number;
+  colorOuterImageName: string;
+  colorInnerId: number;
+  colorInnerImage: string;
+  colorCarInnerImage: string;
+  colorInnerPrice: number;
+  colorInnerImageName: string;
 }
 
 interface IItemContext {
@@ -69,6 +95,7 @@ const initialSelectedItem = {
     name: '',
     title: '',
     imgSrc: '',
+    carImgSrc: '',
     price: 0,
   },
   outerColor: {
@@ -89,40 +116,10 @@ const initialItem: IItemContext = {
   setTotalPrice: () => {},
 };
 
-type actionType =
-  | { type: 'SET_TRIM'; value: defaultItemType }
-  | { type: 'SET_POWER_TRAIN'; value: detailItemType }
-  | { type: 'SET_OPERATION'; value: detailItemType }
-  | { type: 'SET_BODY_TYPE'; value: detailItemType }
-  | { type: 'SET_INNER_COLOR'; value: detailItemType }
-  | { type: 'SET_OUTER_COLOR'; value: IOuterColorItemType }
-  | { type: 'SET_OPTIONS'; value: detailItemType[] };
-
-const reducer = (state: ISelectedItem, action: actionType): ISelectedItem => {
-  switch (action.type) {
-    case 'SET_TRIM':
-      return { ...state, trim: action.value };
-    case 'SET_POWER_TRAIN':
-      return { ...state, modelType: { ...state.modelType, powerTrain: action.value } };
-    case 'SET_OPERATION':
-      return { ...state, modelType: { ...state.modelType, operation: action.value } };
-    case 'SET_BODY_TYPE':
-      return { ...state, modelType: { ...state.modelType, bodyType: action.value } };
-    case 'SET_INNER_COLOR':
-      return { ...state, innerColor: action.value };
-    case 'SET_OPTIONS':
-      return { ...state, options: action.value };
-    case 'SET_OUTER_COLOR':
-      return { ...state, outerColor: action.value };
-    default:
-      return state;
-  }
-};
-
 export const ItemContext = createContext(initialItem);
 
 export default function ItemProvider({ children }: IItemProvider) {
-  const [selectedItem, setSelectedItem] = useReducer(reducer, initialSelectedItem);
+  const [selectedItem, setSelectedItem] = useReducer(itemReducer, initialSelectedItem);
   const [totalPrice, setTotalPrice] = useState<number>(0);
 
   return (
---
+++ frontend/src/reducer/itemReducer.ts
@@ -0,0 +1,36 @@
+import {
+  IColorItemType,
+  ISelectedItem,
+  defaultItemType,
+  detailItemType,
+} from '../context/ItemProvider';
+
+type actionType =
+  | { type: 'SET_TRIM'; value: defaultItemType }
+  | { type: 'SET_POWER_TRAIN'; value: detailItemType }
+  | { type: 'SET_OPERATION'; value: detailItemType }
+  | { type: 'SET_BODY_TYPE'; value: detailItemType }
+  | { type: 'SET_INNER_COLOR'; value: IColorItemType }
+  | { type: 'SET_OUTER_COLOR'; value: IColorItemType }
+  | { type: 'SET_OPTIONS'; value: detailItemType[] };
+
+export default function itemReducer(state: ISelectedItem, action: actionType): ISelectedItem {
+  switch (action.type) {
+    case 'SET_TRIM':
+      return { ...state, trim: action.value };
+    case 'SET_POWER_TRAIN':
+      return { ...state, modelType: { ...state.modelType, powerTrain: action.value } };
+    case 'SET_OPERATION':
+      return { ...state, modelType: { ...state.modelType, operation: action.value } };
+    case 'SET_BODY_TYPE':
+      return { ...state, modelType: { ...state.modelType, bodyType: action.value } };
+    case 'SET_INNER_COLOR':
+      return { ...state, innerColor: action.value };
+    case 'SET_OPTIONS':
+      return { ...state, options: action.value };
+    case 'SET_OUTER_COLOR':
+      return { ...state, outerColor: action.value };
+    default:
+      return state;
+  }
+}
