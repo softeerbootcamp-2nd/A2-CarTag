28244ac1c5cec869450bd1dc3a04e9a0e7776b74
kimdaye77 <63107805+kimdaye77@users.noreply.github.com>
2023-08-23 16:41:23 +0900

    [FEAT] #422: 로컬스토리지 값 불러와서 저장

--- frontend/src/context/ItemProvider.tsx
+++ frontend/src/context/ItemProvider.tsx
@@ -1,6 +1,8 @@
-import { ReactNode, createContext, useEffect, useReducer, useState } from 'react';
+import { ReactNode, createContext, useCallback, useEffect, useReducer, useState } from 'react';
 import itemReducer, { actionType } from '../reducer/itemReducer';
 import { OUTER_COLOR_FIRST_IDX } from '../utils/constants';
+import { SHARE_INFO_API } from '../utils/apis';
+import { ISharedInfo } from '../hooks/useSharedInfo';
 
 export type defaultItemType = {
   id: number;
@@ -118,7 +120,6 @@ const initialSelectedItem = {
     displacement: '',
   },
 };
-
 const initialItem: IItemContext = {
   selectedItem: initialSelectedItem,
   totalPrice: 0,
@@ -129,6 +130,48 @@ const initialItem: IItemContext = {
 export const ItemContext = createContext(initialItem);
 
 export default function ItemProvider({ children }: IItemProvider) {
+  const [data, setData] = useState<ISharedInfo | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+  const isSelectedTrim = localStorage.getItem('isSelectedTrim');
+  const savedItem = localStorage.getItem('selectedItem');
+  const getParams = useCallback(() => {
+    if (!isSelectedTrim || !savedItem || isSelectedTrim === 'false') return;
+    const params = {
+      carId: JSON.parse(savedItem).trimId,
+      powerTrainId: JSON.parse(savedItem).modelTypeId.powerTrain,
+      bodyTypeId: JSON.parse(savedItem).modelTypeId.bodyType,
+      operationId: JSON.parse(savedItem).modelTypeId.operation,
+      outerColorId: JSON.parse(savedItem).outerColorId,
+      innerColorId: JSON.parse(savedItem).innerColorId,
+      optionIdList: JSON.parse(savedItem).optionId ? JSON.parse(savedItem).optionId : [],
+    };
+    return params;
+  }, [isSelectedTrim, savedItem]);
+
+  useEffect(() => {
+    if (!isSelectedTrim || !savedItem || isSelectedTrim === 'false') return;
+    const params = getParams();
+    const fetchData = async () => {
+      try {
+        const res = await fetch(SHARE_INFO_API, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(params),
+        });
+        const jsonData = await res.json();
+        setData(jsonData);
+      } catch (e) {
+        setError(e as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [getParams, isSelectedTrim, savedItem]);
+
   const [selectedItem, setSelectedItem] = useReducer(itemReducer, initialSelectedItem);
   const [totalPrice, setTotalPrice] = useState<number>(0);
 
@@ -155,6 +198,85 @@ export default function ItemProvider({ children }: IItemProvider) {
     localStorage.setItem('selectedItem', JSON.stringify(savedId));
   }, [selectedItem]);
 
+  useEffect(() => {
+    if (!data || loading || error) return;
+    setSelectedItem({
+      type: 'SET_TRIM',
+      value: {
+        id: data.carId,
+        name: data.trim,
+        price: data.carDefaultPrice,
+      },
+    });
+    setSelectedItem({
+      type: 'SET_POWER_TRAIN',
+      value: {
+        id: data.powerTrainId,
+        title: data.powerTrainTitle,
+        name: data.powerTrainName,
+        imgSrc: data.powerTrainImage,
+        price: data.powerTrainPrice,
+      },
+    });
+    setSelectedItem({
+      type: 'SET_BODY_TYPE',
+      value: {
+        id: data.bodyTypeId,
+        title: data.bodyTypeTitle,
+        name: data.bodyTypeName,
+        imgSrc: data.bodyTypeImage,
+        price: data.bodyTypePrice,
+      },
+    });
+    setSelectedItem({
+      type: 'SET_OPERATION',
+      value: {
+        id: data.operationId,
+        title: data.operationName,
+        name: data.operationImage,
+        imgSrc: data.operationImage,
+        price: data.operationPrice,
+      },
+    });
+
+    setSelectedItem({
+      type: 'SET_OUTER_COLOR',
+      value: {
+        id: data.colorOuterId,
+        name: data.colorOuterImageName,
+        title: data.colorOuterTitle,
+        price: data.colorOuterPrice,
+        carImgSrc: data.colorCarOuterImage,
+        imgSrc: data.colorOuterImage,
+      },
+    });
+    setSelectedItem({
+      type: 'SET_INNER_COLOR',
+      value: {
+        id: data.colorInnerId,
+        name: data.colorInnerImageName,
+        title: data.colorInnerTitle,
+        price: data.colorInnerPrice,
+        carImgSrc: data.colorCarInnerImage,
+        imgSrc: data.colorInnerImage,
+      },
+    });
+
+    const options = data.optionList
+      ? data.optionList.map((option) => ({
+          id: option.optionId,
+          name: option.optionName,
+          title: option.optionTitle,
+          price: option.optionPrice,
+          imgSrc: option.optionImage,
+        }))
+      : [];
+
+    setSelectedItem({
+      type: 'SET_OPTIONS',
+      value: options,
+    });
+  }, [data, error, loading]);
   return (
     <ItemContext.Provider
       value={{
