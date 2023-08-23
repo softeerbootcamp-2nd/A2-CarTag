a40289ba3e7f0777be84944ac685003b16c51c07
jijiseong <jiseong5036@gamil.com>
2023-08-23 21:55:47 +0900

    [BUGFIX] #269: BoughtInfo 무한 요청 버그 수정

--- frontend/src/hooks/useSharedInfo.ts
+++ frontend/src/hooks/useSharedInfo.ts
@@ -50,21 +50,23 @@ export default function useSharedInfo() {
   const [error, setError] = useState<Error | null>(null);
   const [searchParams] = useSearchParams();
 
-  const trimId = searchParams.get('trimId');
-  const powerTrainId = searchParams.get('powerTrainId');
-  const bodyTypeId = searchParams.get('bodyTypeId');
-  const operationId = searchParams.get('operationId');
-  const outerColorId = searchParams.get('outerColorId');
-  const innerColorId = searchParams.get('innerColorId');
-  const optionIds = searchParams
-    .get('optionIds')
-    ?.split(',')
-    .map((str) => parseInt(str));
-  const isShared = Boolean(
-    trimId && powerTrainId && bodyTypeId && operationId && outerColorId && innerColorId
-  );
-
   const getParams = useCallback(() => {
+    const trimId = searchParams.get('trimId');
+    const powerTrainId = searchParams.get('powerTrainId');
+    const bodyTypeId = searchParams.get('bodyTypeId');
+    const operationId = searchParams.get('operationId');
+    const outerColorId = searchParams.get('outerColorId');
+    const innerColorId = searchParams.get('innerColorId');
+    const optionIds = searchParams
+      .get('optionIds')
+      ?.split(',')
+      .map((str) => parseInt(str));
+    const isShared = Boolean(
+      trimId && powerTrainId && bodyTypeId && operationId && outerColorId && innerColorId
+    );
+
+    if (!isShared) return null;
+
     const params = {
       carId: trimId,
       powerTrainId,
@@ -75,12 +77,12 @@ export default function useSharedInfo() {
       optionIdList: optionIds ? optionIds : [],
     };
     return params;
-  }, [trimId, powerTrainId, bodyTypeId, operationId, outerColorId, innerColorId, optionIds]);
+  }, [searchParams]);
 
   useEffect(() => {
-    if (!isShared) return;
-
     const params = getParams();
+    if (!params) return;
+
     const fetchData = async () => {
       try {
         const res = await fetch(SHARE_INFO_API, {
@@ -103,7 +105,7 @@ export default function useSharedInfo() {
     };
 
     fetchData();
-  }, [isShared, getParams]);
+  }, [getParams]);
 
   return { data, loading, error };
 }
